22779c3ec46825421d6ef27f5949e7ff14a1ee69
fanihun oluwatobi <only1zizii@gmail.com>
2023-09-14 01:12:07 -0500

    tailwind

---
+++ assignments/tailwind starter/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    container: {
+    }
+  
+    extend: {},
+  },
+  plugins: [],
+}
+
+
